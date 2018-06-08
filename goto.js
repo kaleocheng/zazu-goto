9b38e0d82cc9135fbe27bf817a1a7ed7bae7c824
Kaleo Cheng <kaleocheng@gmail.com>
2018-06-08 22:46:08 +0800

    Read index and vault from file

--- goto.js
+++ goto.js
@@ -1,3 +1,6 @@
+const fs = require('fs')
+const data = JSON.parse(fs.readFileSync('/tmp/zazu-goto-data', 'utf8'))
+
 const grep = (what, where, callback) => {
   let exec = require('child_process').exec
   exec("grep " + what + " " + where, function(err, stdin, stdout) {
@@ -8,7 +11,7 @@ const grep = (what, where, callback) => {
       let l = {
         icon: 'fa-safari',
         title: r,
-        value: r
+        value: data[r]
       }
       list.push(l)
     }
@@ -18,7 +21,8 @@ const grep = (what, where, callback) => {
 
 module.exports = (pluginContext) => {
   return (search, env = {}) => {
-    const cfg = env.cfgPath || '~/.zazu_goto'
+    //const cfg = env.cfgPath || '~/.zazu_goto'
+    const cfg = '/tmp/zazu-goto-index'
     return new Promise((resolve, reject) => {
       grep(search, cfg, (list) => {
         resolve(list)
--- urls.js
+++
@@ -1,7 +0,0 @@
-const urls = {
-  'h': 'https://github.com',
-  'r': 'https://github.com/kaleocheng?tab=repositories',
-  'w': 'https://github.com/orgs/Wiredcraft/dashboard',
-}
-
-module.exports = urls
